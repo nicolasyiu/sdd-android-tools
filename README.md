029034533594605b1de335ae5e9cec9eb95d7339
saxer <15201280641@qq.com>
2015-10-22 15:06:52 +0800

    add v sign

--- README.md
+++ README.md
@@ -20,3 +20,17 @@ $ versionCode	24370000
 $ sdd i p $apkfilepath
 $ com.tencent.mm,apkpath.apk
 ```
+
+```shell
+$ sdd v sign $apkfilepath
+Owner: CN=Tencent, OU=Tencent Guangzhou Research and Development Center, O=Tencent Technology(Shenzhen) Company Limited, L=Shenzhen, ST=Guangdong, C=86
+Issuer: CN=Tencent, OU=Tencent Guangzhou Research and Development Center, O=Tencent Technology(Shenzhen) Company Limited, L=Shenzhen, ST=Guangdong, C=86
+Serial number: 4d36f7a4
+Valid from: Wed Jan 19 22:39:32 CST 2011 until: Fri Jan 11 22:39:32 CST 2041
+Certificate fingerprints:
+	 MD5:  18:C8:67:F0:71:7A:A6:7B:2A:B7:34:75:05:BA:07:ED
+	 SHA1: CC:80:D7:6A:A9:FE:94:EC:20:5E:F0:C3:36:BF:C4:24:59:6D:A2:90
+	 SHA256: 0F:E4:FF:85:C2:15:91:83:96:DA:DC:7C:D8:CE:69:63:33:9A:F3:3D:37:75:1A:56:E5:4C:72:06:B6:3A:3C:7C
+	 Signature algorithm name: SHA1withRSA
+	 Version: 3
+```
--- bin/sdd.sh
+++ bin/sdd.sh
@@ -1,5 +1,6 @@
 VERSION="0.1.1"
 SRC_PATH=$(cd `dirname $0`;cd ../src;pwd) #current shell path
+TMP_PATH=$(cd `dirname $0`;cd ../tmp;pwd) #current shell path
 sdd(){
   if [ $# -lt 1 ];then
     sdd help
@@ -15,6 +16,7 @@ sdd(){
       echo "  sdd --version                         Print out the latest released version of sdd"
       echo "  sdd i <apk-file-path>                 Show app infos"
       echo "  sdd i p <apk-file-path>               Show  app packageName"
+      echo "  sdd v sign <apk-file-path>            Show  app signature"
     ;;
     "--version")
       echo $VERSION
@@ -25,6 +27,20 @@ sdd(){
         $SRC_PATH/i_p.py $3
       else
         $SRC_PATH/i.py $2
+      fi
+    ;;
+    "v")
+      if [ "sign" = $2 ]
+      then
+        sudo rm -r $TMP_PATH/apk_files/* >> /dev/null 2>&1 #delete unused files
+        unzip $3 -d $TMP_PATH/apk_files/ >> /dev/null 2>&1 #unzip apk files to specified directory
+        for rsa in $TMP_PATH/apk_files/META-INF/*.RSA
+        do
+          keytool -printcert -file $rsa
+        done
+        sudo rm -r $TMP_PATH/apk_files/* >> /dev/null 2>&1 #delete unused files
+      else
+        echo 'none'
       fi
     ;;
   esac
